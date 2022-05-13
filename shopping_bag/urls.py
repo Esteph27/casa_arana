eb774b85bc3a1891ed2751f82d06f4b88cf99765
Esteph27 <esteph.csa@gmail.com>
2022-05-13 16:33:46 +0000

    Added adjust bag and remove from bag views, and calc subtotal template filter

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_shopping_bag, name="view_shopping_bag"),
     path('add/<item_id>/', views.add_to_bag, name="add_to_bag"),
+    path('adjust/<item_id>/', views.adjust_bag_qty, name="adjust_bag_qty"),
+    path('remove/<item_id>/', views.remove_from_bag, name="remove_from_bag"),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_shopping_bag(request):
@@ -14,6 +14,7 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else: 
@@ -21,3 +22,35 @@ def add_to_bag(request, item_id):
     
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag_qty(request, item_id):
+    """ Adjust the quantity of sepecific product"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_shopping_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove item from shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
