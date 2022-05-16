e8e8f1151d9a87667f88143704e4c01d1b15e70f
Esteph27 <esteph.csa@gmail.com>
2022-05-16 15:32:27 +0000

    set up toasts - need to add javascript to get them working

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages 
+
+from products.models import Product
 
 
 def view_shopping_bag(request):
@@ -10,15 +13,16 @@ def view_shopping_bag(request):
 def add_to_bag(request, item_id):
     """ Add item to bag and quantity to shopping bag"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else: 
         bag[item_id] = quantity
+        messages.success(request, f'{product.name} added to your bag')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
