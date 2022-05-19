e3b13c63908ef545ca5afdfdb37b5cc90d5b9ce4
Esteph27 <esteph.csa@gmail.com>
2022-05-19 21:02:43 +0000

    Updated css styling for log out page

--- checkout/views.py
+++ checkout/views.py
@@ -5,16 +5,16 @@ from .forms import OrderForm
 
 
 def order_checkout(request):
-    shopping_bag = request.session.get('shopping_bag', {})
-    if not shopping_bag:
-        messages.error(request, "There are no items in your bag at the moment.")
-        return redirect(reverse('products'))
     
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
 
-    order_from = OrderForm()
+    order_form = OrderForm()
     template = 'checkout/order_checkout.html'
     context = {
-        'order_form' = order_from
+        'order_form': order_form,
     }
 
     return render(request, template, context)
