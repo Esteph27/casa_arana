4f5c9243d27a1cff052a2e51c17804cc761ed4d1
Esteph27 <esteph.csa@gmail.com>
2022-05-20 10:33:44 +0000

    added stripe elements js

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def order_checkout(request):
     template = 'checkout/order_checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KjjqeGaYG4N8lqVCgdSemyW9gPGI5Jgzik3zMeQlnowMn4mGkf85BJhTClZJVrW7tusX3C2WhspltnhwdU4DP1800xYVwQ2t1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
