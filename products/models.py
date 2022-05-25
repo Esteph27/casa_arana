36bedf6deb0e6afa3f813289bca4cf0e6b96435a
Esteph27 <esteph.csa@gmail.com>
2022-05-25 15:37:14 +0000

    Added friendly name field to category model and added product form for admin

--- products/models.py
+++ products/models.py
@@ -3,20 +3,27 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    ''' category model '''
-
+    """
+    category model
+    """
+    
     class Meta:
         verbose_name_plural = 'Categories'
 
     name = models.CharField(null=True, blank=True, max_length=200)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
 class Product(models.Model):
-
-    ''' Product info model '''
+    """
+    Product info model
+    """
 
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
