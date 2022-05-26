c8252915527a9d80ae4be698c6e2193685e0fddd
Esteph27 <esteph.csa@gmail.com>
2022-05-26 21:28:35 +0000

    reset database to fix migration error

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
     """
@@ -22,7 +21,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     """
-    Product info model
+    Product information
     """
 
     category = models.ForeignKey(
@@ -30,12 +29,33 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+
     color = models.CharField(max_length=254, null=True, blank=True)
-    dimensions_and_composition = models.TextField()
+    dimensions = models.CharField(max_length=200, null=True, blank=True)
+    composition = models.CharField(max_length=300, null=True, blank=True)
     features = models.CharField(max_length=500, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=False)
 
+    artisan = models.ForeignKey(
+        'Artisan', null=True, blank=False, on_delete=models.SET_NULL, related_name="products")
+
     def __str__(self):
         return self.name
+
+
+class Artisan(models.Model):
+    """
+    Artisan information
+    """
+
+    name = models.CharField(null=True, blank=True, max_length=200)
+    location = models.CharField(null=True, blank=True, max_length=200)
+    story = models.TextField(max_length=1000, blank=True)
+    profile_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
