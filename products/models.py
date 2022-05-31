a11732238854187c9992be765f85ec199b9e067f
Esteph27 <esteph.csa@gmail.com>
2022-05-31 20:35:40 +0000

    updated product price field to fix decimal error

--- products/models.py
+++ products/models.py
@@ -35,7 +35,7 @@ class Product(models.Model):
     dimensions = models.CharField(max_length=200, null=True, blank=True)
     composition = models.CharField(max_length=300, null=True, blank=True)
     features = models.CharField(max_length=500, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=False)
